15d9b4c79e7b455982224144686a4efcdd59b6fd
Rob Wormald <robwormald@gmail.com>
2015-04-07 21:29:19 -0700

    add module decorator

--- src/module.js
+++ src/module.js
@@ -1,5 +1,11 @@
 import angular from 'angular';
  
-export const getModule = moduleName => {
+export const getModule = (moduleName) => {
 	return angular.module(moduleName);
+}
+
+export const ngModule = (moduleConfig) => {
+	return (moduleCtor) => {
+		angular.module(moduleConfig.name,moduleConfig.dependencies);
+	}
 }
