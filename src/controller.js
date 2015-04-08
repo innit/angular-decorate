1a775c04f0dde334ca4696a41417eeab821065bf
Rob Wormald <robwormald@gmail.com>
2015-04-07 21:29:44 -0700

    add controller registration

--- src/controller.js
+++ src/controller.js
@@ -1,15 +1,12 @@
-import angular from 'angular';
 import {getModule} from './module';
 
 export const ngController = (config) => {
   let mod = getModule(config.module);
-  if(!mod){
-    throw new Error(`module ${config.module} not defined!`)
-  }
   return (controllerCtor) => {
     if(config.inject){
       controllerCtor.$inject = config.inject;
     }
+    mod.controller(config.name,controllerCtor);
 
   }
 }
